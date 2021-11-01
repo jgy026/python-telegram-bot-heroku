603d61a29380584e1b077b0d4f44bb9356c9e37d
jgy026 <jayalgru@protonmail.com>
2021-11-01 09:38:29 +0800

    second commit add token

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'YOURTELEGRAMBOTTOKEN'
+TOKEN = '2096972891:AAHdatQMMmGKCg_lJHgzPyTved5Vncy85TY'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
